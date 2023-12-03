805eb3bf0d6f631e528beb899268e449b342b935
Abilash <abilashb2000@gmail.com>
2023-12-03 23:10:56 +0530

    some styles and comments

--- script.js
+++ script.js
@@ -5,12 +5,6 @@ const issueInput = document.querySelector("#input-issue");
 const issueContainer = document.querySelector(".issues-container");
 let issue;
 
-createIssueBtn.addEventListener("click", (e) => {
-  createIssueBtn.classList.add("issuebutton-hide");
-  form.classList.remove("form-hide");
-  issueInput.focus();
-});
-
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -33,6 +27,15 @@ form.addEventListener("submit", (e) => {
   dragStart();
 });
 
+createIssueBtn.addEventListener("click", (e) => {
+  //for toggling between button and input
+  createIssueBtn.classList.add("issuebutton-hide");
+  form.classList.remove("form-hide");
+
+  //automatically focus on input after button clicked
+  issueInput.focus();
+});
+
 //function to attach event listeners in all issues when dragstart triggered
 function dragStart() {
   document.querySelectorAll(".issue").forEach((el) => {
@@ -44,12 +47,20 @@ function dragStart() {
   });
 }
 
+// Adding dragover before drop ensures the browser recognizes the drop location, making your drag-and-drop functionality effective.
+//  Without it, the browser may not acknowledge the drop target.
+
 dropzones.forEach((zone) => {
   zone.addEventListener("dragover", (e) => {
+    // Adding event.preventDefault() in the dragover event is necessary to override the browser's default behavior,
+    // which typically prohibits dropping
     e.preventDefault();
   });
 
   zone.addEventListener("drop", (e) => {
+    // issue will be connected as a children for the container
+    // issue will be disconnected automatically with the body element.
+    // as No two nodes exist in the DOM tree with the same memory reference.
     zone.appendChild(issue);
   });
 });
